c865a6ca2fbe38aa8181484ded929fe41e07e4a3
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-28 14:28:25 +0600

    Add files via upload

---
+++ Доска для крестиков-ноликов/Доска для крестиков-ноликов/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Доска_для_крестиков_ноликов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string first_string = "   |   |   ";
+            string second_string = "---+---+---";
+            Console.WriteLine($"{first_string}\n{second_string}\n{first_string}\n{second_string}\n{first_string}");
+            Console.ReadLine();
+
+        }
+    }
+}
