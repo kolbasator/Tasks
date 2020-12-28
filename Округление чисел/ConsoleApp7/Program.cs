c282f1d8ab17f770dc6cf9fb59fb2ab23581a766
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-28 14:35:17 +0600

    Add files via upload

---
+++ Округление чисел/ConsoleApp7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данная программа округляет числа .Введите число:");
+            string number = Console.ReadLine();
+            double number_int = Convert.ToDouble(number);
+            double result =Math.Round(number_int);
+            Console.WriteLine($"Результат-{result}");
+            Console.ReadLine();
+        }
+    }
+}
