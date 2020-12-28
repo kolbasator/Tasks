858a3ce5cd363621d0c3798ec5e5fe029e38fe1f
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-28 14:31:37 +0600

    Add files via upload

---
+++ Переводчик злотых в доллары/ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это программа для перевода злотых в доллары .Введи кол-во злотых:");
+            string pln = Console.ReadLine();
+            int plnInt = Convert.ToInt32(pln);
+            double dollars = plnInt / 3.67;
+            Console.WriteLine("Результат :");
+            Console.WriteLine(Math.Round(dollars, 2));
+            Console.ReadLine();
+        }
+    }
+}
