ce350feba7d7cb39f3f77944d2ddde155259e321
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-28 15:12:36 +0600

    Add files via upload

---
+++ Вычислялка среднего значения чисел в масиве/Вычислялка среднего значения чисел в масиве/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Вычислялка_среднего_значения_чисел_в_масиве
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers:");
+            string value = Console.ReadLine();
+            double[] array= new double[Convert.ToInt32(value)];
+            Console.WriteLine("put your numbers:");
+            for(int j = 0; j < array.Length; j++)
+            {
+                array[j] = Convert.ToDouble(Console.ReadLine());
+            }
+            double total = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                total = total + array[i];
+            }
+            double average = total / array.Length;
+            Console.WriteLine($"The average of numbers {average}.Thanks for using our program!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Гистограмма/Гистограмма/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Гистограмма
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "";
+            string two = "";
+            string three = "";
+            string four = "";
+            string five = "";
+            Console.WriteLine("How many numbers do you want to enter: ");
+            int value_of_numbers = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[value_of_numbers+=1];
+            for (int j = 1; j < array.Length; j++)
+            {
+                Console.WriteLine($"Enter number {j}");
+                bool h = true;
+                while (h == true)
+                {
+
+                    int array_value = Convert.ToInt32(Console.ReadLine());
+                    if(array_value<=5)
+                    {
+                        array[j] = array_value;
+                        switch (array_value)
+                        {
+                            case 1:
+                                one+="*";
+                                break;
+                            case 2:
+                                two+= "*";
+                                break;
+                            case 3:
+                                three += "*";
+                                break;
+                            case 4:
+                                four += "*";
+                                break;
+                            case 5:
+                                five += "*";
+                                break;
+                        }
+                        h = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect number.Please write number from 1 to 5!");
+                        continue;
+                    }
+                    
+                }
+                
+            }
+            Console.WriteLine($"1:{one}\n2:{two}\n3:{three}\n4:{four}\n5:{five}");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
