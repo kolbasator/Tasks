68f934f6d9fe708e7315f58d66f6c8ee89ad8ee8
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 00:53:26 +0600

    Add files via upload

---
+++ Simplicity check/Проверка числа на простоту/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Проверка_числа_на_простоту
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Эта программа проверяет число на простоту .Введите число:");
+            string NumberString = Console.ReadLine();
+            bool Result = IsPrimeNumber(Convert.ToUInt16(NumberString));
+            if (Result)
+            {
+                Console.WriteLine("Число простое!");
+            }
+            else
+            {
+                Console.WriteLine("Число составное!");
+            }
+            Console.WriteLine("Спасибо за пользование программой!");
+            Console.ReadLine();
+        }
+        public static bool IsPrimeNumber(uint n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
