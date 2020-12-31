f29c5d73a04021c2af3a9a14bee0fbcfc8f1f3b6
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 00:58:35 +0600

    Add files via upload

---
+++ Random numbers generator/Генератор рандомных чисел/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Генератор_рандомных_чисел
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool i = true;
+            while (i)
+            {
+                Random Rand = new Random();
+                Console.WriteLine("Программа для генерации случайных чисел .");
+                Console.WriteLine("Хотите сгенерировать случайное число?Да/Нет");
+                string Move = Console.ReadLine();
+                switch (Move)
+                {
+                    case "Да":
+                        Console.WriteLine(Convert.ToString(Rand.Next(1000000)));
+                        continue;
+                    case "Нет":
+                        i = false;
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
