ddf611a00f6ec32610c170859dbe572ca9d45082
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 01:26:06 +0600

    Add files via upload

---
+++ Struct ,which take user`s Data/Структура для хранения данных пользователя/Me.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Структура_для_хранения_данных_пользователя
+{
+    struct Me
+    {
+        public string Name;
+        public string LastName;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
