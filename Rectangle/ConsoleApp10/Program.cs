6a224fc81298ca043af56866da8a316160fae4af
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 00:45:58 +0600

    Add files via upload

---
+++ Rectangle/ConsoleApp10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{"*****"}\n{"*   *"}\n{"*****"}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rounding numbers/ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данная программа округляет числа .Введите число:");
+            double number_int = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Результат-{Math.Round(number_int)}");
+            Console.ReadLine();
+        }
+    }
+}
