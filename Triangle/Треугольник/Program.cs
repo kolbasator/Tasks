11ba3c74c3fdbe4616a2f8737a38672381f3db1e
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 00:56:11 +0600

    Add files via upload

---
+++ Triangle/Треугольник/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Треугольник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int i;
+            int j;
+            int k;
+            Console.WriteLine("Эта программа нарисует вам треугольник с указанной вами высотой.");
+            Console.Write("Введите высоту : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                for (j = 1; j < num - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
