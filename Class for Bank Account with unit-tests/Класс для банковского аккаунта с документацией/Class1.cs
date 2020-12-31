c456e71783250f9e2b0acee1e21857f67cfe601c
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 01:30:23 +0600

    Add files via upload

---
+++ Class for Bank Account with unit-tests/Класс для банковского аккаунта с документацией/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Class for Bank Account
+    /// </summary>
+
+    public class BankAccount
+    {
+        public int balance = 500;
+        /// /// <summary>
+        /// Method for withdrawing money from an account
+        /// </summary>
+        /// <param name="how_much">How much money to take from the account.</param>
+
+        public void TakeMoney(int how_much)
+        {
+
+            if (this.balance < how_much)
+            {
+                Console.WriteLine("Exception:NotEnoughMoneyException");
+            }
+            else if (how_much < 0)
+            {
+                Console.WriteLine("Exception:NegativeAmountException");
+            }
+            else
+            {
+                this.balance -= how_much;
+            }
+        }
+    }
+}
