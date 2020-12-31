52240c2f310d870dde5b85ccf32fc2a11b56af96
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-01 00:38:47 +0600

    Add files via upload

---
+++ Area of Rectangle/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Данная программа предназначена для высчитывания площади прямоугольникa . \nВведите длину:");
+            string Long= Console.ReadLine();
+            Console.WriteLine("ВВедите ширину :");
+            string Wide = Console.ReadLine();
+            int Result = Int32.Parse(Long) * Int32.Parse(Wide);
+            Console.WriteLine($"Результат - {Result}");
+            Console.ReadLine();
+        }
+    }
+}
